3cdb1458990d5591946e1b938f30b1e0929c0e61
Victor Torrealba <vtorrealbalp@gmail.com>
2024-11-28 03:02:45 -0400

    minor: patent schemas

--- app/models/llm.py
+++ app/models/llm.py
@@ -13,5 +13,40 @@ class Technology(BaseModel):
     disease: List[str] = Field(..., description="The disease for the technology, more than one disease is possible. Only generate diseases that Immunoglobulin Y has the potential to cure")
 
 class PrimaryInvention(BaseModel):
-    text: str = Field(..., description="the redacted primary invention section for the patent draft")
+    prediction: str | None  = Field(..., description="the redacted primary invention section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the primary invention generation")
+
+class FieldOfInvention(BaseModel):
+    prediction: str | None = Field(..., description="the redacted field of invention section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the field of invention generation")
+
+class BackgroundAndNeed(BaseModel):
+    prediction: str | None = Field(..., description="the redacted background and need section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the background and need generation")
+
+class BriefSummary(BaseModel):
+    prediction: str = Field(..., description="the redacted brief summary section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the brief summary generation")
+
+class TechnologyPlatform(BaseModel):
+    prediction: str = Field(..., description="the redacted technology platform section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the technology platform generation")
+
+class DescriptionOfInvention(BaseModel):
+    prediction: str = Field(..., description="the redacted description of invention section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the description of invention generation")
+
+class ProductOrProducts(BaseModel):
+    prediction: str = Field(..., description="the redacted product section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the product generation")
+
+class Uses(BaseModel):
+    prediction: str = Field(..., description="the redacted uses section for the patent draft")
+    trace_id: str = Field(..., description="the langfuse trace id for the uses generation")
+
+
+                            
+
+
+
 
