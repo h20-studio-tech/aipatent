9a21cd36244c5fda47be1191d252b32e6414e0f9
Victor Torrealba <vtorrealbalp@gmail.com>
2025-02-28 11:51:42 -0400

    feat(app): add metadata extraction model for document analysis

---
+++ app/models/metadata_extraction.py
@@ -0,0 +1,18 @@
+import logging 
+import instructor
+import time
+
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+class Extraction(BaseModel):
+    method: list[str] = Field(
+        default_factory=list, 
+        description="method or methods observed/applied in the document. E.g., gene editing, CRISPR, PCR, fermentation, etc")
+    hypothetical_questions: list[str] = Field(
+        default_factory=list,
+        description="Hypothetical questions that this document could answer",
+    )
+    keywords: list[str] = Field(
+        default_factory=list, description="Biology related keywords that this document is about or MeSH headings for p apers"
+    )
